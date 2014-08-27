ac7f8158f9ee626340cef0abb4dc9e6659e267dc
Mitchell Grohoski <mcg218@lehigh.edu>
2014-08-27 16:43:24 +0000

    Mitchell Grohoski github hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//  Mitchell Grohoski
+// CSE2
+//  Welcome Class Java Program - hw01
+
+//  define a class 
+public class WelcomeClass{
+
+//  add main method 
+public static void main(String[] args){
+
+// Welcome message - My LIN - Tweet sized statement
+System.out.println("     -----------");
+System.out.println("     | WELCOME |");
+System.out.println("     -----------");
+System.out.println("   ^  ^  ^  ^  ^  ^ ");
+System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+System.out.println(" |-0--8--8--0--1--7-|");
+System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println("   v  v  v  v  v  v ");
+System.out.println("  Over the summer I went to the beach for a week with my friends down in Ocean City Maryland. There were 14 people in a single house.");
+
+    }
+}
